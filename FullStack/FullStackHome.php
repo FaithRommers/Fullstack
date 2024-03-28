aa862a37efc392999b6a3ec116f7173f35ded037
Faith <rommersfaith@gmail.com>
2024-03-28 10:17:18 +0100

    start van front end

---
+++ FullStack/FullStackHome.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="FullStackCSS.css" /> 
+  <title>CasusCafe</title>
+</head>
+<body>
+
+  <header> 
+    <!-- navigation bar  -->
+    <ul> 
+      <li><a class="active" href="FullStackHome.php">Home</a></li> 
+      <li><a href="FullStackTickets.php">Tickets</a></li> 
+      <li><a href="FullStackAdmin.php">Admin</a></li> 
+      <li style="float:right"><a class="active" href="FullStackInloggen.php">Inloggen</a></li>
+    </ul> 
+  </header>
+
+  <main>
+    <div class="wooooParagraphy"> 
+      <p>EasyTiger Patio heeft een klein cafe/muziektheater genaamd CasusCafe dat muziekavonden organiseert waarop bands op bezoek komen om te spelen. 
+          Op 1 avond is er ruimte voor een hoofdact en een
+          supportact, maar er zijn ook wel eens festivals waarbij er meerdere hoofdacts spelen op een
+          avond of dag.
+          Soms is de toegang gratis, maar in veel gevallen wordt er entreegeld gevraagd. Een band heeft
+          zo zijn eigen prijs.
+          log in of registreer om tickets te bestellen om u favoriete bands live te supporten of een plek te reserveren.
+      </p>
+    </div>
+
+  </main>
+
+
+  <footer> 
+      <p>&copy; CasusCafe 2024</p> 
+  </footer> 
+
+</body>
+</html>
---
+++ FullStack/FullStackInloggen.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="FullStackCSS.css" /> 
+  <title>CasusCafe</title>
+</head>
+<body>
+
+<header>  
+  <!-- navigation bar  -->
+    <ul> 
+      <li><a href="FullStackHome.php">Home</a></li> 
+      <li><a href="FullStackTickets.php">Tickets</a></li> 
+      <li><a href="FullStackAdmin.php">Admin</a></li> 
+      <li style="float:right"><a class="active" href="FullStackInloggen.php">Inloggen</a></li>
+    </ul> 
+</header>
+
+<main>
+
+</main>
+
+<footer> 
+  <p>&copy; CasusCafe 2024</p> 
+</footer> 
+
+</body>
+</html>
